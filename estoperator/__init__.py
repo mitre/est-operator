5459e05bd597ea8dead921859fc288532a8d97ca
T. Miller <cerebus2@gmail.com>
2021-01-18 14:01:16 -0600

    FIPS build version (#11)
    * add enable FIPS mode when envvar OPENSSL_FIPS=1
    
    * restore orginal dockerfile and add FIPS dockerfile
    
    * update README with FIPS mode instructions
    
    * remove [secure] extra from urllib3
    
    * reorganize requirements files for FIPS version
    
    * updated dockerfile for fips version
    
    * updated FIPS mode documentation
    
    * remove doubled shebang
    
    * add pyOpenSSL back, confirmed backend FIPS

--- estoperator/__init__.py
+++ estoperator/__init__.py
@@ -1,5 +1,28 @@
 #!/usr/bin/env python
 
+import ctypes
+import os
+import ssl
+from pathlib import Path
+
+import lddwrap
+
+
+def setup_fips(**kwargs):
+    deps = lddwrap.list_dependencies(path=Path(ssl._ssl.__file__))
+    libcrypto = [x.soname for x in deps if x.soname and "libcrypto" in x.soname]
+    libcrypto = ctypes.CDLL(libcrypto[0])
+    if not libcrypto.FIPS_mode_set(1):
+        raise ssl.SSLError("FIPS mode failed to initialize")
+
+
+# Must do this before importing `cryptography` is imported because it brings in
+# hashlib, and that leads to mode weirdness. Note that hashlib.md5() will still
+# work, but it uses a pure python implementation rather than OpenSSL's.
+if os.getenv("OPENSSL_FIPS") == "1":
+    setup_fips()
+
+
 from .certificaterequest import *
 from .estissuer import *
 from .estorder import *
