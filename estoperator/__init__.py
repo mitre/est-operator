0ae35d9f542af15be921b58f80e1af8fca47328b
Timothy J. Miller <cerebus2@gmail.com>
2021-01-17 18:48:47 -0600

    add enable FIPS mode when envvar OPENSSL_FIPS=1

--- estoperator/__init__.py
+++ estoperator/__init__.py
@@ -1,5 +1,27 @@
 #!/usr/bin/env python
 
+#!/usr/bin/env python
+
+import ctypes
+import os
+import ssl
+from pathlib import Path
+
+import lddwrap
+
+
+def setup_fips(**kwargs):
+    deps = lddwrap.list_dependencies(path=Path(ssl._ssl.__file__))
+    libcrypto = [x.soname for x in deps if x.soname and "libcrypto" in x.soname]
+    libcrypto = ctypes.CDLL(libcrypto[0])
+    if not libcrypto.FIPS_mode_set(1):
+        raise ssl.SSLError("FIPS mode failed to initialize")
+
+
+if os.getenv("OPENSSL_FIPS") == "1":
+    setup_fips()
+
+
 from .certificaterequest import *
 from .estissuer import *
 from .estorder import *
