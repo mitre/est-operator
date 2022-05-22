14c43ad5276079e9bf32a8dfd3e60dcaba645641
Timothy J. Miller <cerebus2@gmail.com>
2022-05-22 13:50:18 -0500

    deprecate a bunch of things

--- est-operator.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-# This script really is nothing more than a shell to import the module. It's
-# just here to give the ~kopf~ command something to chew on.
-
-import estoperator
-
-if __name__ == "__main__":
-    pass
