69fb60bbad9c8be77d9c8d147bc5c2f7dd6703d7
Timothy J. Miller <cerebus2@gmail.com>
2022-05-22 13:45:33 -0500

    basic test framework

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,35 @@
 #!/usr/bin/env python3
 """Suite-wide fixtures."""
 
+from pathlib import Path
+
 import pytest
+from lightkube import codecs
 from lightkube.config.kubeconfig import KubeConfig
 from lightkube.core.client import Client
+from lightkube.resources.apiextensions_v1 import CustomResourceDefinition
+
+BASE = Path("manifests/crds")
+APIS = [
+    "estissuers.est.mitre.org", "estclusterissuers.est.mitre.org",
+    "estorders.est.mitre.org"
+]
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def client():
     return Client(config=KubeConfig.from_env())
+
+
+@pytest.fixture(scope="session")
+def resources(client):
+    # install estissuer, estclusterissuer, estorder
+    for crd in APIS:
+        with open(BASE / f"{crd}.yaml") as f:
+            for obj in codecs.load_all_yaml(f):
+                client.create(obj)
+    yield
+    # delete estissuer, estclusterissuer, estorder
+    for api in APIS:
+        crd = client.get(CustomResourceDefinition, name=api)
+        client.delete(CustomResourceDefinition, name=crd.metadata.name)
--- tests/test_lightkube.py
+++ tests/test_lightkube.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """Test estissuer lightkube resources."""
 
-import estissuer.resources.est_mitre_org_v1alpha1 as estres
 import pytest
 from lightkube.generic_resource import create_namespaced_resource
+
+import estissuer.resources.est_mitre_org_v1alpha1 as estres
+from estissuer.models.est_mitre_org_v1alpha1 import VERSION
 
 ESTISSUER = dict(
     metadata=dict(name="test", ),
@@ -19,20 +21,27 @@ ESTISSUER = dict(
 )
 
 
-@pytest.fixture
-def estissuer_class(client):
-    return create_namespaced_resource(estres.GROUP, estres.VERSION,
-                                      "EstIssuer", "estissuers")
+@pytest.fixture(scope="module")
+def genEstIssuer(client, resources):
+    return create_namespaced_resource(estres.GROUP, VERSION, "EstIssuer",
+                                      "estissuers")
 
 
-@pytest.fixture
-def estissuer(client, estissuer_class):
-    return estissuer_class(ESTISSUER)
+@pytest.fixture(scope="module")
+def gen_test_issuer(client, genEstIssuer):
+    generic = genEstIssuer(**ESTISSUER)
+    client.create(generic)
+    yield
+    client.delete(genEstIssuer, name="test", namespace="default")
 
 
-def test_get_estissuer():
-    # create via generic
-    # get generic
+def test_get_estissuer(client, genEstIssuer, gen_test_issuer):
+    generic = client.get(genEstIssuer, name="test", namespace="default")
+    generic = generic.to_dict()
+    # generic contains apiVersion and kind, which models hide from you
+    del generic["apiVersion"]
+    del generic["kind"]
     # get as estissuer
+    custom = client.get(estres.EstIssuer, name="test", namespace="default")
     # assert same
-    pass
+    assert generic == custom.to_dict()
